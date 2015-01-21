7572554b973d4ccb5bfadc4c98054cdf81e4f4d0
Garrett Graham <ganderson7197@icloud.com>
2015-01-21 16:06:12 -0500

    added to class notes the difference between consoleLog and return. added functions for add and sub

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
-console.log(print(A, B))
+var assert = require('assert');
+
+function add (a, b) {
+	return (a + b); 
+}
+
+assert(add(3, 4) === 7)
+
+function diff(a, b) {
+    return (a - b)
+}
+
+assert(diff(12,-5) === 17)
+
+function prod (a, b) {
+    return (a * b)
+}
+
+assert(prod(4, 9) === 36)
+
+
+
