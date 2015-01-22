bcb422f5d0420bc714313b6c08a54fbda74c12d9
Garrett Graham <ganderson7197@icloud.com>
2015-01-22 07:45:05 -0500

    adding to the if else function for the assignment. dont know if this is correct but i wanted to have something down. again, i get the concept just need to nail the application

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,6 @@ function add (a, b) {
 	return (a + b); 
 }
 
-as
 assert(add(3, 4) === 7)
 
 function diff (a, b) {
@@ -25,10 +24,48 @@ function div (a, b) {
 
 assert(div(3,2) === 1.5)
 
-
+function wordToInt(word) {
+    word = word.toLowerCase ();
+    
+    if(word == 'one') {
+        return 1;
+    }
+    
+    else if (word == 'zero') {
+        return 0;
+    }
+    
+    else if (word == 'two') {
+        return 2;
+    }
+    
+    else if (word == 'three') {
+        return 3;
+    }
+    
+    else if (word == 'four') {
+        return 4;
+    }
+    
+    else if (word == 'five') {
+        return 5;
+    }
+    
+    else if (word == 'six') {
+        return 6;
+    }
+    
+    else if (word == 'seven') {
+        return 7;
+    }
+    
+    assert(plus("zero","zero") === 0);
+    assert(plus("one", "one") === 1);
+    assert(plus("two", "two") === 2);
+    assert
+    
+    
+    
+    
 assert(plus("one", "one") === 2);
 
-if ("one" = 1) {
-    return true
-} else {
-    return false}
