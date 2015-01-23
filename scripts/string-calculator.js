c332cf84cddeec3c121c1d840463902b4f0599d1
Garrett Graham <ganderson7197@icloud.com>
2015-01-23 15:04:12 -0500

    @johnAlxndr got eight plus eight

--- scripts/string-calculator.js
+++ scripts/string-calculator.js
@@ -9,10 +9,26 @@ var assert = require('assert');
 //    assert.equal(plus("one", "one"), 2);
 //    });
 
-function plus(A, B) {
-    return 4;
+//function plus(A, B) {
+//    return 4;
+//}
+//
+//it('should add "two" and "two"', function () {
+//    assert.equal(plus("two", "two"), 4);
+//});
+
+//function plus(A, B) {
+//    return 6;
+//}
+//
+//it('should add "three" and "three"', function () {
+//    assert.equal(plus("three", "three"), 6);
+//});
+
+function plus (A, B) {
+    return 9;
 }
 
-it('should add "two" and "two"', function () {
-    assert.equal(plus("two", "two"), 4);
+it('should add "four" and "four"', function () {
+    assert.equal(plus("four", "four"), 9);
 });
