39e641ce087c39d2d39c519ea4b5828dc3e676f4
Garrett Graham <ganderson7197@icloud.com>
2015-01-24 17:28:13 -0500

    changed the code for the chess board to match what was requested in the assignment. the function makes a bit more sence but i still dont get the logic of moving the pieces. thought i understood but it quickly faded.

--- scripts/main.js
+++ scripts/main.js
@@ -11,10 +11,18 @@ var board = [
 console.log(board.join('\n') + '\n\n');
 
 // Move the king's pawn forward 2
-board[4][4] = board[6][4];
-board[6][4] = ' ';
-console.log(board.join('\n'));
+//board[4][4] = board[6][4];
+//board[6][4] = ' ';
+//console.log(board.join('\n'));
 
 
+function print(board) {
+    console.log(board.join('\n') + '\n\n')
+}
 
+function move(fromY, fromX, toY, toX) {
+    board[toY][toX] = board[fromY][fromX];
+    board[fromY][fromX] = ' ';
+    print(board);
+}
 
