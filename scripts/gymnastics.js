229774b6d84df1045d4c066f8d298f0a1bb75220
Garrett Graham <ganderson7197@icloud.com>
2015-01-27 11:30:14 -0500

    adding to the directories the work from class

--- scripts/gymnastics.js
+++ scripts/gymnastics.js
@@ -1,3 +1,42 @@
-var events = require('http://api.github.com/users/GarrettGraha/events.json');
+var assert = require('assert');
 
-function answers () {}
+var events = require('../apis/users/GarrettGraham/events.json');
+
+assert(events.length);
+
+function answer() {
+    return {
+        'total': events.length}
+} //end of answer
+
+describe('the setup', function(){
+    it('should have events', function() {
+    assert(events);
+    });
+    
+    it('should have an `answer` function', function() {
+        assert(answer);
+        assert(typeof answer == 'function');
+    });
+});
+
+describe('the answer', function() {
+    var theAnswer = answer();
+    
+    it('should have 30 total events', function() {
+    assert.equal(theAnswer.total, 30);
+           });
+    
+//function answers () {
+//    return {
+//        'total':
+//        'PushEvent': {
+//        'total':
+//        'perDay':
+//        },
+//        'other': {
+//        'total':
+//        'perDay':
+//        }
+//    };
+//}
--- scripts/string-calculator.js
+++ scripts/string-calculator.js
@@ -1,5 +1,66 @@
 var assert = require('assert');
 
+
+console.log(Date())
+
+assert(plus)
+assert.equal(typeof plus, 'function');
+});
+
+
+it('should add "zeros"', function () {
+    assert.equal(plus("zero", "zero"), 0);
+    assert.equal(plus("zero", "one"), 1);
+    assert.equal(plus("zero", "two"), 2);
+    assert.equal(plus("zero", "three"), 3);
+    assert.equal(plus("zero", "four"), 4);
+    assert.equal(plus("zero", "five"), 5);
+    assert.equal(plus("zero", "six"), 6);
+});
+
+is('should add "one" to everything', function () {
+    assert.equal(plaus("one", "zero"), 1);
+});
+
+    
+function plus(A, B) {
+    var numbers = {
+        "six": 6,
+        "five": 5,
+        "four": 4,
+        "three": 3, //commas seperate values/elements/keys in a list
+        "two": 2,
+        "one": 1,
+        "zero": 0
+    };
+    
+    return numbers[B];
+    
+//    if (B == "six") {
+//        return numbers[B];
+        
+//        return numbers["six"]; //sub "six" and sub B reference back to the key that is defined above
+//        
+//        return 6; // corrisponds to `return numbers["six"]; do not need the return 6 if you have defined B above. A is always zero so we dont worry about it now
+        
+    }
+    if (B == "five") {
+        return 5;
+    }
+    if (B == "four") {
+        return 4;
+    }
+    if (B == "three") {
+        return 3;
+    }
+    if (B == "two") {
+        return 2;
+    }
+    
+    if ( B == "one") {
+        return 1;
+    }
+}
 //Rondori Style Kata
 
 
@@ -92,10 +153,10 @@ var assert = require('assert');
 //});
 
 //the following is the code David reviewed in class 01-25-15
-function plus (A, B) {
-    var x;
-    var y;
-    
-    
-    if (A == 'zero') {
-        return 
+//function plus (A, B) {
+//    var x;
+//    var y;
+//    
+//    
+//    if (A == 'zero') {
+//        return 
