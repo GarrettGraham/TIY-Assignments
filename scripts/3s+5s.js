d14bf744507520bf535718d62b11b10c69ecfb28
Garrett Graham <ganderson7197@icloud.com>
2015-01-28 17:14:54 -0500

    got the 3s and 5s to pass each test.  this was only possible piggy backing off of the abilities of the collective whole of the cohort. this is a bit more undrstandable as the loop is created to sift through a bunch of numbers. honestly this still isnt clicking and i feel very inept but im staying positive. im hoping that the info will click the more i see how a task is accomplished and then reverse engineering the code. otherwise im at a loss

--- scripts/3s+5s.js
+++ scripts/3s+5s.js
@@ -1,3 +1,23 @@
+var assert = require('assert');
+
+function solution (x) {
+
+var sum = 0;
+    for(i = 0; i < x; i++) {
+        if (i % 3 === 0 || i % 5 === 0){
+            sum+=i;
+        }
+    }
+    return sum;
+}
+
+console.log(solution(20));
+console.log(solution(30));
+console.log(solution(50));
+console.log(solution(100));
+console.log(solution(1000));
+
+
 it('should have a `solution` function', function(){
   assert(solution);
   assert.equal(typeof solution, 'function');
@@ -5,16 +25,20 @@ it('should have a `solution` function', function(){
 
 it('should have a `solution` for (10, 20, 30)', function(){
   assert.equal(solution(10), 23);
-  assert.equal(solution(20), undefined);
-  assert.euqal(solution(30), undefined);
+  assert.equal(solution(20), 78);
+  assert.euqal(solution(30), 195);
 });
 
 it('should have a `solution` for 50', function(){
-  assert.equal(solution(50), undefined);
+  assert.equal(solution(50), 543);
 });
 
 it('should have a solution for 100', function(){
-  assert.equal(solution(100), undefined);
+  assert.equal(solution(100), 2318);
+});
+
+it('should have a solution for 1000', function(){
+  assert.equal(solution(1000), 233168);
 });
 
 // What about 1000?
