bab4b4e3d2660731b09f82668401b5b42c3a596e
Garrett Graham <ganderson7197@icloud.com>
2015-01-28 18:57:23 -0500

    added the template code from the assignments directory for the chessboard.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 var board = [
     ['R','N','B','Q','K','B','N','R'],
     ['P','P','P','P','P','P','P','P'],
@@ -8,7 +10,30 @@ var board = [
     ['p','p','p','p','p','p','p','p'],
     ['r','n','b','q','k','b','n','r']];
 
-console.log(board.join('\n') + '\n\n');
+function print(board){
+  // Before any rows are printed, print the `<table>`...
+  console.log('<table class="chessboard">');
+
+  _.forEach(board, function(row, rank){
+
+      // Before any cells are printed...
+
+      _.forEach(row, function(square, file){
+
+          // In here, we have `rank`, `file`, and `square`...
+
+      }); // END foreach(row)
+
+      // After all the cells are printed...
+
+  }); // END foreach(board)
+
+  // After all the rows are printed...
+  console.log('</table> <!-- .chessboard -->');
+} // END print
+
+
+//console.log(board.join('\n') + '\n\n');
 
 // Move the king's pawn forward 2
 //board[4][4] = board[6][4];
@@ -16,14 +41,14 @@ console.log(board.join('\n') + '\n\n');
 //console.log(board.join('\n'));
 
 
-function print(board) {
-    console.log(board.join('\n') + '\n\n')
-}
-
-function move(fromY, fromX, toY, toX) {
-    board[toY][toX] = board[fromY][fromX];
-    board[fromY][fromX] = ' ';
-    print(board);
-}
-
-function move(8, 4, 7, 6); 
+//function print(board) {
+//    console.log(board.join('\n') + '\n\n')
+//}
+//
+//function move(fromY, fromX, toY, toX) {
+//    board[toY][toX] = board[fromY][fromX];
+//    board[fromY][fromX] = ' ';
+//    print(board);
+//}
+//
+//function move(8, 4, 7, 6); 
