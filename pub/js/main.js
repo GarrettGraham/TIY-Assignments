94fb0f1cbef653ddda98a7bbf03dfb47c74c58e7
Garrett Graham <ganderson7197@icloud.com>
2015-02-08 23:15:40 -0500

    coded the function for populating the json data and the post function but none of it is working and i cant figure it out.  i think the jquery syntax is correct but im not calling anything correctly im sure which is no help with the multitude of div classes

--- pub/js/main.js
+++ pub/js/main.js
@@ -5,8 +5,17 @@ $(document).ready(function(data) {
     });
     
     $.getJSON("octocat.json", function(user) {
+        $('.profileHeader').find('img').append(user.avatar_url),
         $('.profileHeader').find('h4').append(user.name),
-        $('.profileHeader').find('h5').append(data.login), //carolina said to call the data.login in order to populate the h5 but jared wrote user.login
-$('.profileHeader').find('
-        
+        $('.profileHeader').find('h5').append(data.login), //carolina said to call the data.login in order to populate the h5 but jared wrote user.login. which is it?
+        $('.profileHeader').find('').append(user.company),
+        $('profileHeader').find('').append(user.email),
+        $('profileHeader').find('').append(user.blog),
+        $('.profileHeader').find('').append(user.followers),
+        $('
     });
+
+//$('button').click(function (postComment) {
+//    $.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/248/comments?access_token=74bb82cbc3e64f93285bab5934430939524b8472", comment)
+//    });
+        //pretty sure this isnt correct
