55d3e96e3809aa19e906db2cfd593f53567a0128
Garrett Graham <ganderson7197@icloud.com>
2015-02-09 18:04:45 -0500

    i downloaded an h5bp into my master on my local before i switched to a branch to do it that way. i didnt push into my master until Logan helped me backtrack to a previous commit but that did not have the complete index js and css files from the assignment 20. i saved the files in a sublime text file and i will push this and then merge it back into my master.

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,3 +1,5 @@
+// !?!?!MUST have the apis folder withing the pub folder(the document root) in order for the jquery functions to work correctly and for the data to be populated within the index.html file!?!?!
+
 $(document).ready(function(data) {
 //                console.log()
 // saw that Jared wrote a test for the json data. id like to understand that more: use watch mocha in terminal to run the test? looking at the code is a helpful reminder of the syntax of jquery and he explained to me the classes being called but im still fuzzy on the function names, like he wrote user and i wrote users. is there a difference?
@@ -28,4 +30,60 @@ $(document).ready(function(data) {
 //                    
 //    Adam did $.getJSON
 //                    $('').html
+        
+/*        
+===========================================================================
+Class Notes about the Assignment 20
+===========================================================================
 
+!!The below script code is for the API info to be ued for the assignment
+    I still need to play major catch up in order to understand the DOM, JSOn
+    and the API stuff and synthesize the knowledge and commit it to memory!! 
+
+        
+    $(document).ready(function() {
+
+        var userData = $.getJSON("https://api.github.com/users/adamKarb");
+
+        userData.done(function(json) {
+        var user =json;
+
+        $("#myname").html(user.name); ::this it the code from Adam; this is the code i want to do ONCE i get the data
+        $(#myusername").html(user.login);
+});
+
+$('.main-tabs a[href="#repositories"]').on('click', function() {
+    //populate the repositories JUST IN TIME...
+    userData.done(function(user){ 
+    $.getJSON(user.repos_url).done(function(repos) {
+        }); // on(click, .main-tabs a[href="#repositories"])
+    });
+});
+    <script>
+        var octocat;
+            
+            var promise = $.get('https://api.github.com/users/octocat',
+                function(data) {
+                    console.log('done!',data.login);
+                    octocat = data;
+                }
+            );
+            
+            console.log(octocat);
+            
+            promise.done(function(data) {
+                console.log('second callback', data);
+            });
+
+            promise.done(function() {
+                console.log('third callback', octocat.login);
+            });
+        
+        </script>
+        
+-->
+        
+
+
+*/
+        
