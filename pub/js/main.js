2c3b32d82fb75d28ef22b0b2504e331dc30b14d1
Garrett Graham <ganderson7197@icloud.com>
2015-02-08 21:39:41 -0500

    working on the data population of the page but cant seem to figure out how to call the various bits of info.  the code of others is not much help as i do not know how to populate the page appropriately. i dont really know what is wrong as i have looked on the code of others and i seem to have the source code links but perhaps not.

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function(data) {
+//                console.log()
+// saw that Jared wrote a test for the json data. id like to understand that more: use watch mocha in terminal to run the test? looking at the code is a helpful reminder of the syntax of jquery and he explained to me the classes being called but im still fuzzy on the function names, like he wrote user and i wrote users. is there a difference?
+                
+    });
+    
+    $.getJSON("octocat.json", function(user) {
+        $('.profileHeader').find('h4').append(user.name),
+        $('.profileHeader').find('h5').append(data.login), //carolina said to call the data.login in order to populate the h5 but jared wrote user.login
+$('.profileHeader').find('
+        
+    });
