7bc1765c9609cccdd291bb9158a43f948cb1e0c2
Garrett Graham <ganderson7197@icloud.com>
2015-02-10 10:23:09 -0500

    commented out the JavaScript file as it isnt my code and does look similar to Jareds code and im guessing the code of others but i am just trying to emphasize that i cam not trying to take credit for code that isnt mine and also document that i still need a lot more practice but more so, i need to understand how it all works before i can be expected to create a substantial product that meets the requirements of any further assignment

--- pub/js/main.js
+++ pub/js/main.js
@@ -23,6 +23,8 @@
 
 //The code is not rendering because of source code issues but I really need to ask Logan to step me through the source coding with bower so I can commit this to memory. I am writing out the JavaScript a few times to practice which is helping me uderstand the syntax and I feel more confident in reading the code but still very dim on how to produce this from memory
 
+//First Writeout:
+
 //var d, h, m, s, color;
 //function displayTime() {
 //    d = new Date(); 
@@ -43,43 +45,47 @@
 //    setTimeout(displayTime, 1000);
 //}
 
-var d, h, m, s, color;
-function displayTime() {
-    d = new Date();
-    h = d.getHours();
-    m = d.getMinutes();
-    s = d.getSeconds();
-    
-    if(h <= 9) h = '0'+h;
-    if(m <= 9) m = '0'+m;
-    if(s <= 9) s = '0'+s;
-    
-    color = "#"+h+m+s
-    
-    document.body.style.background = color;
-    
-    document.getElementById("hex").innerHTML = color;
-    
-    setTimeout(displayTime, 1000);
-    
-}
+//Second write out:
 
-var d, h, m, s, color;
-function displayTime() {
-    d = new Date();
-    h = d.getHours();
-    m = d.getMinutes();
-    s = d.getSeconds();
-    
-    if(h <= 9) h = '0'+h;
-    if(m <= 9) m = '0'+m;
-    if(s <= 9) s = '0'+s;
-    
-    color = "#"+h+m+s
-    
-    document.body.style.background = color;
-    
-    document.getElementById("hex").innerHTML = color;
-    
-    setTimeout(displayTime, 1000);
-}
+//var d, h, m, s, color;
+//function displayTime() {
+//    d = new Date();
+//    h = d.getHours();
+//    m = d.getMinutes();
+//    s = d.getSeconds();
+//    
+//    if(h <= 9) h = '0'+h;
+//    if(m <= 9) m = '0'+m;
+//    if(s <= 9) s = '0'+s;
+//    
+//    color = "#"+h+m+s
+//    
+//    document.body.style.background = color;
+//    
+//    document.getElementById("hex").innerHTML = color;
+//    
+//    setTimeout(displayTime, 1000);
+//    
+//}
+
+//Third time's a charm:
+
+//var d, h, m, s, color;
+//function displayTime() {
+//    d = new Date();
+//    h = d.getHours();
+//    m = d.getMinutes();
+//    s = d.getSeconds();
+//    
+//    if(h <= 9) h = '0'+h;
+//    if(m <= 9) m = '0'+m;
+//    if(s <= 9) s = '0'+s;
+//    
+//    color = "#"+h+m+s
+//    
+//    document.body.style.background = color;
+//    
+//    document.getElementById("hex").innerHTML = color;
+//    
+//    setTimeout(displayTime, 1000);
+//}
