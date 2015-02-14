371f6d7e7dbf74c2029d04b94a2cfe4304a187e4
Garrett Graham <ganderson7197@icloud.com>
2015-02-14 18:33:02 -0500

    downloaded fresh h5bp and added the starting point for the tests and functions.

---
+++ String_Calculator/js/main.js
@@ -0,0 +1,22 @@
+//### String Calculator Kata
+//
+//Provide a function called `plus` that accepts two `String` arguments that should always be English number words (e.g. one, two, three) and returns a `Number` representing the sum. Use your new `it()` and `describe()` robots from `mocha`. For example:
+
+//javascript
+
+
+var assert = require('assert');
+
+function plus(A, B){
+  // TYPE ALL OF THIS YOURSELF!
+}
+
+it('should add "one" and "one"', function(){
+  assert.equal(plus("one", "one"), 2);
+});
+
+
+//* Provide test cases for `"zero"` through `"ten"`
+//* Just use `if` and `return` statements
+//* You may need to read about [Logical Operators](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators)
+
