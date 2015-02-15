a6dd948553a6284e72801bb75e913b77b1fe79aa
Garrett Graham <ganderson7197@icloud.com>
2015-02-14 20:42:28 -0500

    got five tsts to pass on the check writing

--- Check_Writing/js/main.js
+++ Check_Writing/js/main.js
@@ -1,12 +1,36 @@
 var assert = require('assert');
 
-function plus(A, B){
-  // TYPE ALL OF THIS YOURSELF!
+var number = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
 }
 
-it('should add "one" and "one"', function(){
-  assert.equal(plus("one", "one"), 2);
+
+function toEnglish(a) {
+  return number[a]
+}
+
+it('should equal one', function(){
+    assert.equal(toEnglish(1), "one");
 });
 
+it('should equal two', function() {
+    assert.equal(toEnglish(2), "two");
+});
+
+it('should equal three', function() {
+    assert.equal(toEnglish(3), "three");
+});
+
+it('should equal four', function() {
+    assert.equal(toEnglish(4), "four");
+});
+
+it('should equal five', function() {
+    assert.equal(toEnglish(5), "five");
+});
 //assert.equal(toEnglish(1), "one");
 //assert.equal(toEnglish(2), "two");
