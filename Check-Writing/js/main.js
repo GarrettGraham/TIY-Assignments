91f7823df73336cce7b2658037428894cb2e0b24
Garrett Graham <ganderson7197@icloud.com>
2015-02-15 14:05:04 -0500

    started an addition loop for the check writing but havent done any tests.  the loop is not crashing though which is good. figure ill do similar here as with string calc and just write a loop that doesnt crash with each operator and then tackle another assignment to come back to this one

--- Check-Writing/js/main.js
+++ Check-Writing/js/main.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+//var assert = require('assert');
 
 var number = {
     1: "one",
@@ -20,102 +20,122 @@ var number = {
     17: "seventeen",
     18: "eighteen",
     19: "nineteen",
-    20: "twenty",
-    21: "twenty-one",
-    50: "fifty",
+//    20: "twenty",
+//    21: "twenty-one",
+//    50: "fifty",
 }
 
+var addOperator = 'addition';
+var i = 0;
+var msg = '';
 
-function toEnglish(a) {
-  return number[a]
+// Addition loop
+
+if (addOperator === 'addition') {
+    while (i < 20) {
+        return i;
+    }
 }
 
-it('should equal one', function(){
-    assert.equal(toEnglish(1), "one");
-});
+// Below loop NOT working
+//for (i = 0; i < 19; i++) {
+//    roundNumber = (i + 1);
+//    msg += 'Round ' + roundNumber + ': ';
+//    msg += scores[i] + '<br />';
+//}
 
-it('should equal two', function() {
-    assert.equal(toEnglish(2), "two");
-});
+//function toEnglish(a) {
+//  return number[a]
+//}
+//
+//it('should equal one', function(){
+//    assert.equal(toEnglish(1), "one");
+//});
+//
+//it('should equal two', function() {
+//    assert.equal(toEnglish(2), "two");
+//});
+//
+//it('should equal three', function() {
+//    assert.equal(toEnglish(3), "three");
+//});
+//
+//it('should equal four', function() {
+//    assert.equal(toEnglish(4), "four");
+//});
+//
+//it('should equal five', function() {
+//    assert.equal(toEnglish(5), "five");
+//});
+//
+//it('should equal six', function(){
+//    assert.equal(toEnglish(6), "six");
+//});
+//
+//it('should equal seven', function() {
+//    assert.equal(toEnglish(7), "seven");
+//});
+//
+//it('should equal eight', function() {
+//    assert.equal(toEnglish(8), "eight");
+//});
+//
+//it('should equal nine', function() {
+//    assert.equal(toEnglish(9), "nine");
+//});
+//
+//it('should equal ten', function() {
+//    assert.equal(toEnglish(10), "ten");
+//});
+//
+//it('should equal eleven', function(){
+//    assert.equal(toEnglish(11), "eleven");
+//});
+//
+//it('should equal twelve', function() {
+//    assert.equal(toEnglish(12), "twelve");
+//});
+//
+//it('should equal thirteen', function() {
+//    assert.equal(toEnglish(13), "thirteen");
+//});
+//
+//it('should equal fourteen', function() {
+//    assert.equal(toEnglish(14), "fourteen");
+//});
+//
+//it('should equal fifteen', function() {
+//    assert.equal(toEnglish(15), "fifteen");
+//});
+//
+//it('should equal sixteen', function(){
+//    assert.equal(toEnglish(16), "sixteen");
+//});
+//
+//it('should equal seventeen', function() {
+//    assert.equal(toEnglish(17), "seventeen");
+//});
+//
+//it('should equal eighteen', function() {
+//    assert.equal(toEnglish(18), "eighteen");
+//});
+//
+//it('should equal nineteen', function() {
+//    assert.equal(toEnglish(19), "nineteen");
+//});
+//
+//it('should equal twenty', function() {
+//    assert.equal(toEnglish(20), "twenty");
+//});
+//
+//it('should equal twenty-one', function() {
+//    assert.equal(toEnglish(21), "twenty-one");
+//});
+//
+//it('should equal fifty', function () {
+//   assert.equal(toEnglish(50), "fifty"); 
+//});
 
-it('should equal three', function() {
-    assert.equal(toEnglish(3), "three");
-});
 
-it('should equal four', function() {
-    assert.equal(toEnglish(4), "four");
-});
-
-it('should equal five', function() {
-    assert.equal(toEnglish(5), "five");
-});
-
-it('should equal six', function(){
-    assert.equal(toEnglish(6), "six");
-});
-
-it('should equal seven', function() {
-    assert.equal(toEnglish(7), "seven");
-});
-
-it('should equal eight', function() {
-    assert.equal(toEnglish(8), "eight");
-});
-
-it('should equal nine', function() {
-    assert.equal(toEnglish(9), "nine");
-});
-
-it('should equal ten', function() {
-    assert.equal(toEnglish(10), "ten");
-});
-
-it('should equal eleven', function(){
-    assert.equal(toEnglish(11), "eleven");
-});
-
-it('should equal twelve', function() {
-    assert.equal(toEnglish(12), "twelve");
-});
-
-it('should equal thirteen', function() {
-    assert.equal(toEnglish(13), "thirteen");
-});
-
-it('should equal fourteen', function() {
-    assert.equal(toEnglish(14), "fourteen");
-});
-
-it('should equal fifteen', function() {
-    assert.equal(toEnglish(15), "fifteen");
-});
-
-it('should equal sixteen', function(){
-    assert.equal(toEnglish(16), "sixteen");
-});
-
-it('should equal seventeen', function() {
-    assert.equal(toEnglish(17), "seventeen");
-});
-
-it('should equal eighteen', function() {
-    assert.equal(toEnglish(18), "eighteen");
-});
-
-it('should equal nineteen', function() {
-    assert.equal(toEnglish(19), "nineteen");
-});
-
-it('should equal twenty', function() {
-    assert.equal(toEnglish(20), "twenty");
-});
-
-it('should equal twenty-one', function() {
-    assert.equal(toEnglish(21), "twenty-one");
-});
-
-it('should equal fifty', function () {
-   assert.equal(toEnglish(50), "fifty"); 
-});
 //assert.equal(toEnglish(1), "one");
 //assert.equal(toEnglish(2), "two");
