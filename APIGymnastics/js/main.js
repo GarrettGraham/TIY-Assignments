e1e9467b44ba539bec5a4ab6adb39b5e0191d914
Garrett Graham <ganderson7197@icloud.com>
2015-02-15 19:42:33 -0500

    cracking the apis

--- APIGymnastics/js/main.js
+++ APIGymnastics/js/main.js
@@ -65,22 +65,114 @@
                 //* _returns:_ `Boolean` whether `callback` returns `true` for _every_ element
                 //* _example:_
 
+//<!-- Jared's code below: i want to use it as a reference and dissect it
 
 
-var events = require('../apis/github/users/GarrettGraham/events.json'); // Replace USERNAME!
+//var _ = require('lodash');
+//var events = require('./github/users/jaredstevick/events.json');
+//var assert = require('assert');
+//
+//var pushEvents = events.filter(function(item){
+//	return item.type == 'PushEvent'
+//});
+//
+//var pullRequests = events.filter(function(item){
+//    return item.type == 'PullRequestEvent';
+//});
+//
+//var issueComment = events.filter(function(item){
+//    return item.type == 'IssueCommentEvent';
+//});
+//
+//var createEvent = events.filter(function(item){
+//	return item.type == 'CreateEvent'
+//});
+//
+//var otherEvents = events.filter(function(item){
+//	return item.type != 'PushEvent'
+//});
+//
+////when did all this happen
+//function dateCreated(a){ 
+//	return _.map(a, 'created_at')
+//};
+//
+////thanks taylor, this totally works
+//function findSpan(a, b){
+//	return _.first(a).substring(0, 10).localeCompare(_.last(b).substring(0, 10)) + 1;
+//}
+//
+////now find the average
+//function avg(item) {
+//	var diff = findSpan(dateCreated(events), dateCreated(events));
+//	return item.length / diff
+//}
+//
+//function theAnswer(){
+//   	return {
+//    'total': events.length,
+//    'PushEvent': {
+//    	'total': pushEvents.length,
+//       	'perDay': avg(pushEvents)
+//    },
+//    'PullRequests': {
+//    	'total': pullRequests.length,
+//    	'perDay': avg(pullRequests)
+//    },
+//    'IssueComment': {
+//    	'total': issueComment.length,
+//    	'perDay': avg(issueComment)
+//    },
+//	'CreateEvents': {
+//		'total': createEvent.length,
+//		'perDay': avg(createEvent)
+//	},
+//     'OtherEvents': {
+//       'total': otherEvents.length,
+//       'perDay': avg(otherEvents)
+//     }
+//   };
+// }; // END answer 
+//
+//it('should have 30 total events', function() {
+//    assert(events.length === 30);
+//    });
+//
+//it('should have 14 `PushEvent` entries', function(){
+//    assert(pushEvents.length === 14);
+//    });
+//
+//console.log(events.length);
+//console.log(pushEvents.length);
+//console.log(pullRequests.length);
+//console.log(issueComment.length);
+//console.log(otherEvents.length);
+//console.log(dateCreated(events));
+//console.log(theAnswer(events));
+//-->
+//
+
+
+
+
+
+var events = require('../apis/github/users/GarrettGraham/events2.json'); // Replace USERNAME!
+var pushEvents = 'PushEvent';
+
 
 function answer(){
   return {
-    'total': events.length;    // How many total events did you fetch?
-    'PushEvent': {
-      'total': ...,  // How many total events of type `PushEvent` are there?
-      'perDay': ...  // On average, how many`PushEvent` entries per day?
-    },
-    'other': {
-      'total': ...,  // How many _other_ events are in the data?
-      'perDay': ...  // How many per day, on average?
-    }
-  };
+    'total': events.length  // How many total events did you fetch?
+      'PushEvent': PushEvent.length 
+      {
+//      'total': ...,  // How many total events of type `PushEvent` are there?
+//      'perDay': ...  // On average, how many`PushEvent` entries per day?
+//    },
+//    'other': {
+//      'total': ...,  // How many _other_ events are in the data?
+//      'perDay': ...  // How many per day, on average?
+//    }
+//  };
 } 
 
 //
