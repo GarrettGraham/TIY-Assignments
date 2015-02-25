c85a13134d64f11c5988315f537bce636ecf8aae
Garrett Graham <ganderson7197@icloud.com>
2015-02-25 08:35:26 -0500

    working on the video assignments. transcribing the answers but i am unable to checkout my attempts.  the button is still loading and the program shows no proof of completion of the assignments.

--- pub/js/app.js
+++ pub/js/app.js
@@ -1,18 +1,67 @@
-(function(){
+(function () {
 
     var app = angular.module('gemStrore', []);
-    
-app.controller('StoreController', function() {
-    this.product = gem;
-});
 
-    var gem = { 
-        name: 'Azurite', 
-        price: 110.50, 
+    app.controller('StoreController', function () {
+        this.product = gem;
+    });
+
+    var gem = {
+        name: 'Azurite',
+        price: 110.50,
         canPurchase: false,
         soldOut: true
     };
 })();
 
 
+//1.7 
 
+(function () {
+    var app = angular.module('gemStore', []);
+
+    app.controller('StoreController', function () {
+        this.products = gems;
+    });
+
+    var gems = [
+        {
+            name: 'Azurite',
+            price: 2.95
+        },
+        {
+            name: 'Bloodstone',
+            price: 5.95
+        },
+        {
+            name: 'Zircon',
+            price: 3.95
+        },
+    ];
+})();
+
+//2.2
+
+(function () {
+
+    var app = angular.module('gemStore', []);
+
+    app.controller('StoreController', function () {
+        this.products = gems;
+    });
+
+    var gems = [
+        {
+            name: 'Azurite',
+            price: 110.50
+        },
+        {
+            name: 'Bloodstone',
+            price: 22.90
+        },
+        {
+            name: 'Zircon',
+            price: 1100
+        },
+    ];
+})();
