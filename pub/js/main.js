a8ec22ebd4e886a87fc8ff365df6abec8ba0891f
Garrett Graham <ganderson7197@icloud.com>
2015-02-25 18:48:51 -0500

    bower installed angular but wasnt able to get the wiredep to sync on brackets for tiy assignments repo but now realize its because i did not do the correct src code which should be wiredep -s src backslash then the file. need to merge this branch and create an angularize me branch. also been adding to the transcriptions of the videos

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,1 +1,46 @@
+////2.5
+//(function () {
+//    var app = angular.module('gemStore', []);
+//
+//    app.controller('StoreController', function () {
+//        this.products = gems;
+//    });
+//
+//    var gems = [{
+//        name: 'Azurite',
+//        description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+//        shine: 8,
+//        price: 110.50,
+//        rarity: 7,
+//        color: '#CCC',
+//        faces: 14,
+//        images: []
+//  }, {
+//        name: 'Bloodstone',
+//        description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+//        shine: 9,
+//        price: 22.90,
+//        rarity: 6,
+//        color: '#EEE',
+//        faces: 12,
+//        images: [
+//      "images/gem-01.gif",
+//      "images/gem-03.gif",
+//      "images/gem-04.gif"
+//    ]
+//  }, {
+//        name: 'Zircon',
+//        description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+//        shine: 70,
+//        price: 1100,
+//        rarity: 2,
+//        color: '#000',
+//        faces: 6,
+//        images: [
+//      "images/gem-06.gif",
+//      "images/gem-07.gif",
+//      "images/gem-09.gif"
+//    ]
+//  }];
+//})();
 
