a3deff4181fb8da3d2871943e3a5ed809b9c91a4
Garrett Graham <ganderson7197@icloud.com>
2015-07-09 14:37:30 -0400

    working on getting the ng repeat working and included the ng controller and ng ap and the ng repeat to the index and did what i can on the js file but dont think its correct. keep getting an error of angular is not defined

--- Assignments-Helluva-Ride/Timeline/js/main.js
+++ Assignments-Helluva-Ride/Timeline/js/main.js
@@ -2,5 +2,16 @@
 var app = angular.module('Timeline', []);
 
 app.controller ('CommitsCtrl', ['$http', function($http){
-  $http.get(js/)
-}])
+  var self = this;
+
+  $http.get(js/commits.json).success(function(data) {
+    this.PushEvent = []; //get info from json?
+    angular.forEach(data, function(PushEvent){
+      this.PushEvent.push({
+        text: PushEvent.commits.message
+      });
+    });
+    console.log(this.commits.message);
+  });
+}
+])
