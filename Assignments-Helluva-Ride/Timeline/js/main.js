7c3c5f00ef43ba6d97a1685f98a1202010805ded
Garrett Graham <ganderson7197@icloud.com>
2015-07-09 13:19:54 -0400

    included the jquery for the popover effect on the nav links and included the appropriate bootstrap classes in the index. also been playing around with the css to get the time stamp to display correctly in the medium viewport size. uncommented the window.jquery script at the bottom of the index file;

--- Assignments-Helluva-Ride/Timeline/js/main.js
+++ Assignments-Helluva-Ride/Timeline/js/main.js
@@ -1,5 +1,3 @@
-// var back = document.getElementByClass('.backToPortfolio');
-// back.dataset.info = Back to Previous;
-
-var character = document.getElementByClass('.backToPortfolio');
-    character.dataset.info = Back to previous;
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover();
+});
