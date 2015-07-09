71d513fdce52b4f00cc84997d241c5618100a8e6
Garrett Graham <ganderson7197@icloud.com>
2015-07-09 16:56:49 -0400

    bulldogging this shiz to get the ng repeat working. the message is printing to the console but not binding with the html and trying to figure out why.

--- Assignments-Helluva-Ride/Timeline/js/main.js
+++ Assignments-Helluva-Ride/Timeline/js/main.js
@@ -6,8 +6,9 @@ app.controller ('CommitsCtrl', ['$http', function($http){
   this.PushEvents = [];
   $http.get('js/commits.json').success(function(data) {
     // self.PushEvents = []; //get info from json?
-    self.PushEvents = data;
-    console.log(self.PushEvents);
+    self.PushEvents = data[0].payload.commits;
+    console.log(self.PushEvents[0].message);
+
     // angular.forEach(data[0].payload.commits, function(PushEvent){
     //   self.PushEvents.push(
     //     {text: PushEvent.message});
