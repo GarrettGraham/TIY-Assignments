8d85ef930345b76f8a2ce1fb71b89ebfdea54d47
Garrett Graham <ganderson7197@icloud.com>
2015-07-10 13:30:12 -0400

    finally got something working. the ul is repeating 30 times but not necessarily the commit objects. it is repeating through the entire array with each of the objects called pushevents but i would like to iterate the commits and populate the model with the commit messages and the created at times

--- Assignments-Helluva-Ride/Timeline/js/main.js
+++ Assignments-Helluva-Ride/Timeline/js/main.js
@@ -1,26 +1,38 @@
 var app = angular.module('Timeline', []);
 
-app.controller ('CommitsCtrl', ['$http', function($http){
-  var self = this;
-  // this.PushEvents = [];
-  $http.get('js/commits.json').success(function(data) {
-    // self.PushEvents = []; //get info from json?
-    self.data = data;
-    console.log(data.payload)
-    // angular.forEach(function(data){
-    //   data.push(payload.commits)
-    // });
-    // self.PushEvents = data[0].payload.commits;
+app.controller ('CommitsCtrl', ['$scope', '$http',
+function($scope, $http){
+  $http.get('js/commits.json').success(function(data){
+    $scope.pushevents = [];
+    console.log(data)
+    angular.forEach(data, function(pushevent){
+      $scope.pushevents.push(pushevent.payload);
+      console.log(pushevent.payload.commits)
+    });
 
-    // console.log(self.PushEvents[0].message);
-    // angular.forEach(data[0].payload.commits, function(PushEvent){
-    //   self.PushEvents.push(
-    //     {text: PushEvent.message});
-    //      });
-    // console.log(self.PushEvents);
   });
-}
-])
+}])
+////app.controller ('CommitsCtrl', ['$http', function($http){
+  ////var self = this;
+    // this.PushEvents = [];
+  ////$http.get('js/commits.json').success(function(data) {
+      // self.PushEvents = []; //get info from json?
+    ////self.data = data;
+    ////console.log(data.payload)
+      // angular.forEach(function(data){
+      //   data.push(payload.commits)
+      // });
+      // self.PushEvents = data[0].payload.commits;
+
+      // console.log(self.PushEvents[0].message);
+      // angular.forEach(data[0].payload.commits, function(PushEvent){
+      //   self.PushEvents.push(
+      //     {text: PushEvent.message});
+      //      });
+      // console.log(self.PushEvents);
+////   });
+//// }
+//// ])
 
 // app.controller ('CommitsCtrl', ['$http', function($http){
 //   var self = this;
