4ba622405a46f833a06ffcb81db382dacd0fb723
Garrett Graham <ganderson7197@icloud.com>
2015-07-10 11:53:18 -0400

    changed the controller notation in the html to data in commitsctrl.data and working the angular controller to get the json data to bind with the model but still having trouble understanding how the naming conventions

--- Assignments-Helluva-Ride/Timeline/js/main.js
+++ Assignments-Helluva-Ride/Timeline/js/main.js
@@ -1,14 +1,18 @@
-
 var app = angular.module('Timeline', []);
 
 app.controller ('CommitsCtrl', ['$http', function($http){
   var self = this;
-  this.PushEvents = [];
+  // this.PushEvents = [];
   $http.get('js/commits.json').success(function(data) {
     // self.PushEvents = []; //get info from json?
-    self.PushEvents = data[0].payload.commits;
-    console.log(self.PushEvents[0].message);
+    self.data = data;
+    console.log(data.payload)
+    // angular.forEach(function(data){
+    //   data.push(payload.commits)
+    // });
+    // self.PushEvents = data[0].payload.commits;
 
+    // console.log(self.PushEvents[0].message);
     // angular.forEach(data[0].payload.commits, function(PushEvent){
     //   self.PushEvents.push(
     //     {text: PushEvent.message});
@@ -17,6 +21,20 @@ app.controller ('CommitsCtrl', ['$http', function($http){
   });
 }
 ])
+
+// app.controller ('CommitsCtrl', ['$http', function($http){
+//   var self = this;
+//   $http.get('js/commits.json').success(function(data) {
+//     self.PushEvents = data[0].payload.commits;
+//     console.log(self.PushEvents[0].message);
+//     angular.forEach(data[0].payload.commits, function(PushEvent){
+//       self.PushEvents.push(
+//         {text: PushEvent.message});
+//          });
+//     console.log(self.PushEvents);
+//     });
+//   }
+//   ])
 
 // app.controller ('CommitsCtrl', ['$scope', '$http',
 // function($scope, $http) {
